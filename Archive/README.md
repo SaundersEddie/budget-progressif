3451086bcae5895cc7dad3cd627ca350c5fd753c
Eddie Saunders <saunders.eddie@outlook.com>
2020-07-01 16:07:52 -0400

    Added shields to readme, code cleanup

---
+++ Archive/README.md
@@ -0,0 +1,10 @@
+# budget-progressif
+
+[![GitHub Repo](https://img.shields.io/github/repo-size/SaundersEddie/Budget-Tracker?color=Green&style=plastic)](https://github.com/SaundersEddie/budget-progressif)
+[![Main Repo Language](https://img.shields.io/github/languages/top/SaundersEddie/Budget-Tracker?color=blueviolet&style=plastic)](https://github.com/SaundersEddie/budget-progressif)
+[![Repo Languages](https://img.shields.io/github/languages/count/SaundersEddie/Budget-Tracker?color=red&style=plastic)](https://github.com/SaundersEddie/budget-progressif)
+[![License](https://img.shields.io/github/license/SaundersEddie/Budget-Tracker?color=yellow&style=plastic)](https://github.com/SaundersEddie/budget-progressif)
+
+It's a budget, that's progressive.
+
+https://online-offline-budgetrechner.herokuapp.com/
--- server.js
+++ server.js
@@ -1,37 +1,32 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const compression = require('compression')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const app = express();
+const app = express()
 
-app.use(logger("dev"));
+app.use(logger('dev'))
 
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(compression())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.use(express.static("public"));
+app.use(express.static('public'))
 
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
+  process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines'
+mongoose.connect(MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
-  useFindAndModify: false,
-});
-
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-// });
+  useFindAndModify: false
+})
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'))
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+  console.log(`App running on port ${PORT}!`)
+})
