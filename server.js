c954638d4d24c0609b8cfd9675509adbf8402136
Eddie Saunders <saunders.eddie@outlook.com>
2020-07-01 17:59:34 -0400

    Prod Candidate

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
-const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines'
-mongoose.connect(MONGODB_URI)
+//const MONGODB_URI =
+//  process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines'
+//mongoose.connect(MONGODB_URI)
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
