aa630bd7aa5a21c6239cbcfe987396ab7fd0e8ba
Eddie Saunders <saunders.eddie@outlook.com>
2020-07-01 16:56:28 -0400

    Code Cleanup, migrate to ES6

--- Archive/public/index.js
+++ Archive/public/index.js
@@ -1,177 +1,177 @@
-let transactions = [];
-let myChart;
+let transactions = []
+let myChart
 
-// Create our indexedDB
-const request = window.indexedDB.open("offLineBudget", 1);
-
-request.onupgradeneeded = ({ target }) => {
-  const db = target.result;
-  const objectStore = db.createObjectStore("offLineBudget");
-  objectStore.createIndex("name", "name");
-  objectStore.createIndex("value", "value");
-  objectStore.createIndex("date", "date");
-};
-
-request.onsuccess = (event) => {
-  console.log(request.result);
-};
-
-fetch("/api/transaction")
-  .then((response) => {
-    return response.json();
+fetch('/api/transaction')
+  .then(response => {
+    return response.json()
   })
-  .then((data) => {
+  .then(data => {
     // save db data on global variable
-    transactions = data;
+    transactions = data
 
-    populateTotal();
-    populateTable();
-    populateChart();
-  });
+    populateTotal()
+    populateTable()
+    populateChart()
+  })
 
-const saveRecord = (record) => {
-  // create a transaction on the pending db with readwrite access
-  const transaction = offlinedb.transaction(["budget"], "readwrite");
-  // access your pending object store
-  const budgetPending = transaction.objectStore("budget");
-  // add record to your store with add method.
-  budgetPending.add(record);
-};
-
-function populateTotal() {
+const populateTotal = () => {
   // reduce transaction amounts to a single total value
   let total = transactions.reduce((total, t) => {
-    return total + parseInt(t.value);
-  }, 0);
+    return total + parseInt(t.value)
+  }, 0)
 
-  let totalEl = document.querySelector("#total");
-  totalEl.textContent = total;
+  let totalEl = document.querySelector('#total')
+  totalEl.textContent = total
 }
 
-function populateTable() {
-  let tbody = document.querySelector("#tbody");
-  tbody.innerHTML = "";
+const populateTable = () => {
+  let tbody = document.querySelector('#tbody')
+  tbody.innerHTML = ''
 
-  transactions.forEach((transaction) => {
+  transactions.forEach(transaction => {
     // create and populate a table row
-    let tr = document.createElement("tr");
+    let tr = document.createElement('tr')
     tr.innerHTML = `
       <td>${transaction.name}</td>
       <td>${transaction.value}</td>
-    `;
+    `
 
-    tbody.appendChild(tr);
-  });
+    tbody.appendChild(tr)
+  })
 }
 
-function populateChart() {
+const populateChart = () => {
   // copy array and reverse it
-  let reversed = transactions.slice().reverse();
-  let sum = 0;
+  let reversed = transactions.slice().reverse()
+  let sum = 0
 
   // create date labels for chart
-  let labels = reversed.map((t) => {
-    let date = new Date(t.date);
-    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-  });
+  let labels = reversed.map(t => {
+    let date = new Date(t.date)
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+  })
 
   // create incremental values for chart
-  let data = reversed.map((t) => {
-    sum += parseInt(t.value);
-    return sum;
-  });
+  let data = reversed.map(t => {
+    sum += parseInt(t.value)
+    return sum
+  })
 
   // remove old chart if it exists
   if (myChart) {
-    myChart.destroy();
+    myChart.destroy()
   }
 
-  let ctx = document.getElementById("myChart").getContext("2d");
+  let ctx = document.getElementById('myChart').getContext('2d')
 
   myChart = new Chart(ctx, {
-    type: "line",
+    type: 'line',
     data: {
       labels,
       datasets: [
         {
-          label: "Total Over Time",
+          label: 'Total Over Time',
           fill: true,
-          backgroundColor: "#6666ff",
-          data,
-        },
-      ],
-    },
-  });
+          backgroundColor: '#6666ff',
+          data
+        }
+      ]
+    }
+  })
 }
 
-function sendTransaction(isAdding) {
-  let nameEl = document.querySelector("#t-name");
-  let amountEl = document.querySelector("#t-amount");
-  let errorEl = document.querySelector(".form .error");
+const sendTransaction = isAdding => {
+  let nameEl = document.querySelector('#t-name')
+  let amountEl = document.querySelector('#t-amount')
+  let errorEl = document.querySelector('.form .error')
 
   // validate form
-  if (nameEl.value === "" || amountEl.value === "") {
-    errorEl.textContent = "Missing Information";
-    return;
+  if (nameEl.value === '' || amountEl.value === '') {
+    errorEl.textContent = 'Missing Information'
+    return
   } else {
-    errorEl.textContent = "";
+    errorEl.textContent = ''
   }
 
   // create record
   let transaction = {
     name: nameEl.value,
     value: amountEl.value,
-    date: new Date().toISOString(),
-  };
+    date: new Date().toISOString()
+  }
 
   // if subtracting funds, convert amount to negative number
   if (!isAdding) {
-    transaction.value *= -1;
+    transaction.value *= -1
   }
 
   // add to beginning of current array of data
-  transactions.unshift(transaction);
+  transactions.unshift(transaction)
 
   // re-run logic to populate ui with new record
-  populateChart();
-  populateTable();
-  populateTotal();
+  populateChart()
+  populateTable()
+  populateTotal()
 
   // also send to server
-  fetch("/api/transaction", {
-    method: "POST",
+  fetch('/api/transaction', {
+    method: 'POST',
     body: JSON.stringify(transaction),
     headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json",
-    },
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    }
   })
-    .then((response) => {
-      return response.json();
+    .then(response => {
+      return response.json()
     })
-    .then((data) => {
+    .then(data => {
       if (data.errors) {
-        errorEl.textContent = "Missing Information";
+        errorEl.textContent = 'Missing Information'
       } else {
         // clear form
-        nameEl.value = "";
-        amountEl.value = "";
+        nameEl.value = ''
+        amountEl.value = ''
       }
     })
-    .catch((err) => {
+    .catch(err => {
       // fetch failed, so save in indexed db
-      saveRecord(transaction);
+      saveRecord(transaction)
 
       // clear form
-      nameEl.value = "";
-      amountEl.value = "";
-    });
+      nameEl.value = ''
+      amountEl.value = ''
+    })
 }
 
-document.querySelector("#add-btn").onclick = function () {
-  sendTransaction(true);
-};
+document.querySelector('#add-btn').onclick = () => {
+  sendTransaction(true)
+}
 
-document.querySelector("#sub-btn").onclick = function () {
-  sendTransaction(false);
-};
+document.querySelector('#sub-btn').onclick = () => {
+  sendTransaction(false)
+}
+
+// request.onupgradeneeded = ({ target }) => {
+//   const db = target.result;
+//   const objectStore = db.createObjectStore("offLineBudget");
+//   objectStore.createIndex("name", "name");
+//   objectStore.createIndex("value", "value");
+//   objectStore.createIndex("date", "date");
+// };
+
+// request.onsuccess = (event) => {
+//   console.log(request.result);
+// };
+
+// Create our indexedDB
+// const request = window.indexedDB.open('offLineBudget', 1)
+
+// const saveRecord = record => {
+//   // create a transaction on the pending db with readwrite access
+//   const transaction = offlinedb.transaction(['budget'], 'readwrite')
+//   // access your pending object store
+//   const budgetPending = transaction.objectStore('budget')
+//   // add record to your store with add method.
+//   budgetPending.add(record)
+// }
--- Archive/public/service-worker.js
+++ Archive/public/service-worker.js
@@ -1,188 +1,62 @@
-// EXS 22nd june 2020 - Our service worker file to cache our page to allow offline data entry.
+const FILES_TO_CACHE = ['/', '/index.html', 'index.js', 'db.js', 'styles.css']
 
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "index.js",
-  "db.js",
-  "styles.css",
-  //"../models/transaction.js",
-  "icons/icon-192x192.png",
-  "icons/icon-512x512.png",
-];
+const CACHE_NAME = 'static-cache-v2'
+const DATA_CACHE_NAME = 'data-cache-v1'
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
-console.log("Our Service Worker");
-
-// EXS 22nd June 2020 - Building our indexedDB if need be
-
-// install
-
-self.addEventListener("install", (event) => {
+//install
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Your files were pre-cached successfully!')
+      return cache.addAll(FILES_TO_CACHE)
     })
-  );
-  self.skipWaiting();
-});
+  )
 
-self.addEventListener("activate", (evt) => {
+  self.skipWaiting()
+})
+
+//activate
+self.addEventListener('activate', evt => {
   evt.waitUntil(
-    caches.keys().then((keyList) => {
+    caches.keys().then(keyList => {
       return Promise.all(
-        keyList.map((key) => {
+        keyList.map(key => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
+            console.log('Removing old cache data', key)
+            return caches.delete(key)
           }
         })
-      );
+      )
     })
-  );
-  self.clients.claim();
-});
+  )
 
-self.addEventListener("fetch", (evt) => {
-  if (evt.request.url.includes("/api/")) {
+  self.clients.claim()
+})
+
+// fetch
+self.addEventListener('fetch', evt => {
+  if (evt.request.url.includes('/api/')) {
     evt.respondWith(
       caches
         .open(DATA_CACHE_NAME)
-        .then((cache) => {
+        .then(cache => {
           return fetch(evt.request)
-            .then((response) => {
+            .then(response => {
               // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
+                cache.put(evt.request.url, response.clone())
               }
-              return response;
+
+              return response
             })
-            .catch((err) => {
+            .catch(err => {
               // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
+              return cache.match(evt.request)
+            })
         })
-        .catch((err) => console.log(err))
-    );
+        .catch(err => console.log(err))
+    )
 
-    return;
+    return
   }
-});
-
-// // create a new db request for a "budget" database.
-// const request = indexedDB.open("budget", 1);
-// request.onupgradeneeded = function (event) {
-//   // create object store called "pending" and set autoIncrement to true
-//   const db = event.target.result;
-//   db.createObjectStore("pending", { autoIncrement: true });
-// };
-// request.onsuccess = function (event) {
-//   db = event.target.result;
-//   // check if app is online before reading from db
-//   if (navigator.onLine) {
-//     checkDatabase();
-//   }
-// };
-// request.onerror = function (event) {
-//   console.log("Woops! " + event.target.errorCode);
-// };
-
-// function checkDatabase() {
-//   // open a transaction on your pending db
-//   const transaction = db.transaction(["pending"], "readwrite");
-//   // access your pending object store
-//   const store = transaction.objectStore("pending");
-//   // get all records from store and set to a variable
-//   const getAll = store.getAll();
-//   getAll.onsuccess = function () {
-//     if (getAll.result.length > 0) {
-//       fetch("/api/transaction/bulk", {
-//         method: "POST",
-//         body: JSON.stringify(getAll.result),
-//         headers: {
-//           Accept: "application/json, text/plain, */*",
-//           "Content-Type": "application/json",
-//         },
-//       })
-//         .then((response) => response.json())
-//         .then(() => {
-//           // if successful, open a transaction on your pending db
-//           const transaction = db.transaction(["pending"], "readwrite");
-//           // access your pending object store
-//           const store = transaction.objectStore("pending");
-//           // clear all items in your store
-//           store.clear();
-//         });
-//     }
-//   };
-// }
-
-// // EXS End of our indexedDB build
-
-// // install
-// self.addEventListener("install", function (evt) {
-//   evt.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       console.log("Your files were pre-cached successfully!");
-//       return cache.addAll(FILES_TO_CACHE);
-//     })
-//   );
-
-//   self.skipWaiting();
-// });
-
-// self.addEventListener("activate", function (evt) {
-//   evt.waitUntil(
-//     caches.keys().then((keyList) => {
-//       return Promise.all(
-//         keyList.map((key) => {
-//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-//             console.log("Removing old cache data", key);
-//             return caches.delete(key);
-//           }
-//         })
-//       );
-//     })
-//   );
-
-//   self.clients.claim();
-// });
-
-// // fetch
-// self.addEventListener("fetch", function (evt) {
-//   // cache successful requests to the API
-//   if (evt.request.url.includes("/api/")) {
-//     evt.respondWith(
-//       caches
-//         .open(DATA_CACHE_NAME)
-//         .then((cache) => {
-//           return fetch(evt.request)
-//             .then((response) => {
-//               // If the response was good, clone it and store it in the cache.
-//               if (response.status === 200) {
-//                 cache.put(evt.request.url, response.clone());
-//               }
-
-//               return response;
-//             })
-//             .catch((err) => {
-//               // Network request failed, try to get it from the cache.
-//               return cache.match(evt.request);
-//             });
-//         })
-//         .catch((err) => console.log(err))
-//     );
-
-//     return;
-//   }
-
-//   // if the request is not for the API, serve static assets using "offline-first" approach.
-//   // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-//   evt.respondWith(
-//     caches.match(evt.request).then(function (response) {
-//       return response || fetch(evt.request);
-//     })
-//   );
-// });
+})
